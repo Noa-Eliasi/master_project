3e29e053de5f657382cc4b5ba853ba91a82b5f0f
Noa Eliasi <noaelisi@gmail.com>
2024-12-23 13:08:11 +0200

    dndnc c

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 ## calculating the fourier sum and the gaussians on the lattice, and plotting them ##
 x, y = np.meshgrid(np.linspace(-4, 4, 300), np.linspace(-4, 4, 300))
 sigma = 0.18
-n, m = 3, 3
+n, m = 2, 2
 
 Z = fnc.gaussian(x, y, 1, n, m, sigma)
 fourier_sum = fnc.fourier_series(1, sigma, n, m, x, y)
